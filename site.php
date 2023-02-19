62195d99e530ca9d27e58c020d719a433f84fb38
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-02-19 11:31:06 -0800

    numbers

--- site.php
+++ site.php
@@ -8,24 +8,42 @@
 </head>
 <body>
     <?php
-    //Variables
-    $characterName = "John Jingle";
-    $characterAge = 35;
-    //Working with strings
-    echo "The characters name is $characterName, his age is $characterAge \n";
-    echo "<br> strtolower: ";
-    echo strtolower($characterName);
-    echo "<br> strtoupper: ";
-    echo strtoupper($characterName);
-    echo "<br> string array char selection: ";
-    echo $characterName[0];
-    echo "<br> Replace sequence of characters: ";
-    echo str_replace("John", "Rodrigo", $characterName);
-    echo "<br> Replace sequence of characters: ";
-    echo substr($characterName, 5);
+        //Variables ------------------------------------------->
+        $characterName = "John Jingle";
+        $characterAge = 35;
+        //Working with strings ------------------------------------------->
+        echo "The characters name is $characterName, his age is $characterAge \n";
+        echo "<br> strtolower: ";
+        echo strtolower($characterName);
+        echo "<br> strtoupper: ";
+        echo strtoupper($characterName);
+        echo "<br> string array char selection: ";
+        echo $characterName[0];
+        echo "<br> Replace sequence of characters: ";
+        echo str_replace("John", "Rodrigo", $characterName);
+        echo "<br> Replace sequence of characters: ";
+        echo substr($characterName, 5);
+        echo "<br>";
+        //Working with numbers ------------------------------------------->
+        echo 5 + 9;
+        echo "<br>";
+        $num = 10.4;
+        $num++;
+        echo $num;
+        echo round($num);
+        echo floor($num);
+        echo ceil($num);
+        echo "<br>";
+        //Working with inputs ------------------------------------------->        
+        
+
     
 
     ?>
+    <form action="site.php" method="get">
+        Name: <input type="text" name="name">
+        <button type="submit"></button>
+    </form>
 </body>
 </html>
 
